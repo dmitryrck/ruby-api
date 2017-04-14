76e5a1de28abcea9a8738925abdf5c634e28a0ae
Dmitry Rocha <dmitryrck@gmail.com>
2017-04-14 08:52:19 -0300

    Add create and update actions to title.

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -3,4 +3,29 @@ class TitlesController < ApplicationController
     @titles = Title.all
     render json: @titles
   end
+
+  def create
+    @title = Title.new(title_params)
+    if @title.save
+      render json: @title
+    else
+      render json: @title.errors, status: :bad_request
+    end
+  end
+
+  def update
+    @title = Title.find(params[:id])
+
+    if @title.update(title_params)
+      render json: @title
+    else
+      render json: @title.errors, status: :bad_request
+    end
+  end
+
+  protected
+
+  def title_params
+    params.permit(:title, :kind_id, :production_year)
+  end
 end
